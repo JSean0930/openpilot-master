64d3e109a21c006bd82745ea92444aa0c9013f54
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2024-11-02 00:06:12 +0800

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -25,7 +25,7 @@ ALLOW_THROTTLE_THRESHOLD = 0.5
 MIN_ALLOW_THROTTLE_SPEED = 2.5
 
 # Lookup table for turns
-_A_TOTAL_MAX_V = [1.7, 3.2]
+_A_TOTAL_MAX_V = [2.04, 3.84] #[1.7, 3.2]
 _A_TOTAL_MAX_BP = [20., 40.]
 
 
