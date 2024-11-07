b4e031119849eef08a2fa32de6f537bbad0231ef
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2024-11-08 00:27:13 +0800

    Update settings.h

--- selfdrive/ui/qt/offroad/settings.h
+++ selfdrive/ui/qt/offroad/settings.h
@@ -101,16 +101,3 @@ private:
   Params params;
   ParamWatcher *fs_watch;
 };
-
-class TimpilotPanel : public QWidget {
-  Q_OBJECT
-
-private:
-  QStackedLayout* main_layout = nullptr;
-  QWidget* home = nullptr;
-
-  QWidget* home_widget;
-
-public:
-  explicit TimpilotPanel(QWidget *parent = nullptr);
-};
